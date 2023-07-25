fdcc0ddf5426ef08f7d1d889a80e590199f05c4e
Alexis <alexisgenieve@gmail.com>
2023-07-25 08:22:03 -0600

    Initial Commit

---
+++ AlexisWrightCapstone/AlexisWrightCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexisWrightCapstoneApp.swift
+//  AlexisWrightCapstone
+//
+//  Created by Alexis Wright on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AlexisWrightCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
